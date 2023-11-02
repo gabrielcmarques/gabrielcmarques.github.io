d4d70265197ac7543ee1cf96b887341010254f3d
Gabriel de Christo Marques <gabrielchristomarques@gmail.com>
2023-11-02 20:47:56 -0300

    borderFix

--- src/routes/skills/+page.svelte
+++ src/routes/skills/+page.svelte
@@ -22,7 +22,7 @@
 
 <div class="flex flex-col items-center justify-center mt-[10vh] max-sm:mt-[5vh]">
 	<h1 class="text-4xl md:text-5xl">Conhecimentos</h1>
-	<div class="max-w-screen-lg border">
+	<div class="max-w-screen-lg">
 		<div class="gap-5 w-full h-full">
 			<h2 class="text-center text-2xl md:text-3xl my-4 max-sm:my-1">Stack Principal:</h2>
 			<div id="one-panel">
