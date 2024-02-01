443d8e7b91b532191f49bddde8ef41087129b734
Gabriel <gabrielchristomarques@gmail.com>
2024-02-01 12:40:56 -0300

    .
    ;

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@ const config = {
 			fallback: 'error.html'
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
