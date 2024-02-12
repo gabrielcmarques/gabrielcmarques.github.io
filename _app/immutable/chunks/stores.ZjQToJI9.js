347d58a8dd4b59809ba6fcc2ec565cb53735cd2e
Gabriel Christo <gabrielchristomarques@gmail.com>
2024-02-12 13:18:42 -0300

    Updates

---
+++ _app/immutable/chunks/stores.ZjQToJI9.js
@@ -0,0 +1,1 @@
+import{s as e}from"./entry.SdZyatci.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
---
+++ _app/immutable/entry/start.YISpreq0.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.SdZyatci.js";export{t as start};
